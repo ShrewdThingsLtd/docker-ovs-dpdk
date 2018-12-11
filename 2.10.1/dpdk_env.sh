3fa2fbc0bb3f8d06341b871f4bfac096066447ff
root <erez@shrewdthings.com>
2018-12-11 04:24:06 -0500

    re-enable F16C to check on physical srvr

--- 2.10.1/dpdk_env.sh
+++ 2.10.1/dpdk_env.sh
@@ -4,7 +4,7 @@ echo "Setting DPDK environment variables..."
 export DPDK_DIR=/usr/src/dpdk
 export DPDK_TARGET=x86_64-native-linuxapp-gcc
 export DPDK_BUILD=$DPDK_DIR/$DPDK_TARGET
-export EXTRA_CFLAGS+=-mno-f16c
+#export EXTRA_CFLAGS+=-mno-f16c
 echo "$EXTRA_CFLAGS"
 
 

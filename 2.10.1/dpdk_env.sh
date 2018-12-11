94aaf82ab62d0f44e02d0c38621bc65186ae715f
Yotam Ramon <yotam@ethernitynet.com>
2018-12-11 11:38:34 -0500

    add sample tgt credentials

--- 2.10.1/dpdk_env.sh
+++ 2.10.1/dpdk_env.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export TGT_IP=172.17.0.1
+export TGT_USER=root
+export TGT_PASS=devops123
 echo "Setting DPDK environment variables..."
 export DPDK_DIR=/usr/src/dpdk
 export DPDK_TARGET=x86_64-native-linuxapp-gcc

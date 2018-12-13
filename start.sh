98da4b7a4fbcbd18163726df0c8210aeed2a8e00
Yotam Ramon <yotam@ethernitynet.com>
2018-12-13 13:54:40 -0500

    remove directory check because Dockerfile moved to root

--- start.sh
+++ start.sh
@@ -4,12 +4,6 @@ DPDK_VERSION=${1:-v17.11-rc4}
 OVS_IMG=${2:-local}
 OVS_VERSION=${3:-v2.10.1}
 
-if [ ! -d "./$OVS_VERSION" ]
-then
-	echo "unsupported OVS_VERSION: $OVS_VERSION"
-	exit -1
-fi
-
 docker volume rm $(docker volume ls -qf dangling=true)
 #docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
